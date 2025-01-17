851058ddbeed03f7bb66b03aec42c5761c53573a
Yordan Romeev <yordan.romeev@gmail.com>
2025-01-17 14:23:08 +0200

    Add Tasks 3 and 4

--- 02. SQL Joins - SirmaAcademyJoinsDB Queries.sql
+++ 02. SQL Joins - SirmaAcademyJoinsDB Queries.sql
@@ -13,6 +13,18 @@ FROM Orders AS o
 
 SELECT od.OrderID, p.ProductName, p.Price, od.Quantity
 FROM OrderDetails AS od
-JOIN Products AS p ON od.ProductID = p.ProductID
+    JOIN Products AS p ON od.ProductID = p.ProductID
 
+-- ### 3. Orders with Shipper Information
+-- Write a query to display the `OrderID`, `OrderDate`, and `ShipperName` for each order. Use the `Orders` and `Shippers` tables to connect the data.
 
+SELECT o.OrderID, o.OrderDate, s.ShipperName
+FROM Orders AS o
+    JOIN Shippers AS s ON o.ShipperID = s.ShipperID
+
+-- ### 4. Products by Category with Prices
+-- Write a query to list all `ProductName`, `CategoryName`, and `Price`. Use the `Products` and `Categories` tables to fetch the category information for each product.
+
+SELECT p.ProductName, c.CategoryName, p.Price
+FROM Products p
+    JOIN Categories c ON p.CategoryID = c.CategoryID
